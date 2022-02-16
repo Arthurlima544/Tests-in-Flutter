7d9dcc99ce1eb319a6fee9144f20c16c5ff5c3f6
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-16 15:31:19 -0300

    Adding TextStyles and ui colors for generalization

---
+++ lib/utils/theme/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:testinflutter/utils/theme/ui_colors.dart';
+
+class TextStyles {
+  static final titleHome = GoogleFonts.lexendDeca(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: UiColors.heading,
+  );
+}
---
+++ lib/utils/theme/ui_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UiColors {
+  static const primary = Color(0xFF610578);
+  static const secondary = Color(0xFF12AAB4);
+  static const heading = Color(0xFFFFFFFF);
+  static const background = Color(0xFFFFFFFF);
+}
