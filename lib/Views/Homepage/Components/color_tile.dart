550a4fd4aa265e0655367e6109fda6e6616ab6b9
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-19 14:33:14 -0300

    refactoring color_tile

--- lib/Views/Homepage/Components/color_tile.dart
+++ lib/Views/Homepage/Components/color_tile.dart
@@ -1,31 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:testinflutter/Models/item.dart';
+import 'package:testinflutter/utils/theme/text_styles.dart';
 import 'package:testinflutter/utils/theme/ui_colors.dart';
 
 class ColorTile extends StatelessWidget {
-  final int index;
-  const ColorTile({
+  final Item item;
+
+  ColorTile({
     Key? key,
-    required this.index,
+    required this.item,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: UiColors.primary,
-      height: 40,
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        children: [
-          Container(
-            height: 40,
-            width: 50,
-            color: Colors.blue,
-          ),
-          Text(
-            "$index",
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 37.0,
+        right: 37.0,
+        top: 40,
+      ),
+      child: Container(
+        color: UiColors.primary,
+        height: 70,
+        width: MediaQuery.of(context).size.width - 37,
+        child: Row(
+          children: [
+            Container(
+              height: 70,
+              width: 70,
+              color: item.color,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Text(
+              "Item ${newRandonNumber(item.number)}",
+              style: TextStyles.titleNormal,
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+int newRandonNumber(int number) {
+  Random random = Random();
+  int res = number == 0 ? random.nextInt(20) : random.nextInt(20) * number;
+  return res;
 }
