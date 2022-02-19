9aaca97b839176f13a1a9e5ee493ac293d6740b1
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-19 14:40:20 -0300

    implementing controller int homepage with provider

--- lib/Views/Homepage/home_page.dart
+++ lib/Views/Homepage/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testinflutter/Controllers/Homepage/list_items_controller.dart';
 import 'package:testinflutter/Views/Homepage/Components/color_tile.dart';
 import 'package:testinflutter/utils/theme/text_styles.dart';
+
+import 'Components/loading_color_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,19 +12,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "App Colors",
-            style: TextStyles.titleHome,
-          ),
+      appBar: AppBar(
+        title: Text(
+          "App Colors",
+          style: TextStyles.titleHome,
         ),
-        body: ListView.builder(
-            itemCount: 100,
-            itemBuilder: (BuildContext context, index) => Padding(
-                  padding: const EdgeInsets.only(bottom: 10, top: 10),
-                  child: ColorTile(
-                    index: index,
-                  ),
-                )));
+      ),
+      body: Selector<ListItemsController, int?>(
+        selector: (context, controller) => controller.pagesLenght,
+        builder: (context, itemCount, child) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              var pagesProvider = Provider.of<ListItemsController>(context);
+
+              // Catalog provides a single synchronous method for getting
+              // the current data.
+              var item = pagesProvider.getByIndex(index);
+
+              if (item.isLoading) {
+                return const LoadingColorTile();
+              }
+
+              return ColorTile(item: item);
+            },
+          );
+        },
+      ),
+    );
   }
 }
