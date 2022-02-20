86f2b15578d661c85291ac319a203576831ab70a
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-19 23:13:41 -0300

    improving Homepage tests

--- test/Views/Homepage/home_page_test.dart
+++ test/Views/Homepage/home_page_test.dart
@@ -4,14 +4,21 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:testinflutter/main.dart';
 
 void main() {
-  group('HomePage testes ', () {
+  group('HomePage tests ', () {
     setUp(() => GoogleFonts.config.allowRuntimeFetching = false);
-    testWidgets("founding at least 1 ColorTile", (tester) async {
+    testWidgets("Should find at least 1 LoadingColorTile", (tester) async {
       await tester.pumpWidget(const MyApp());
-      expect(find.text('Item 0'), findsWidgets);
+      expect(find.text('...'), findsWidgets); // LoadingColorTileWidget
       await tester.fling(find.byType(ListView), const Offset(0, -200), 3000);
       await tester.pumpAndSettle();
-      expect(find.text('Item 0'), findsNothing);
+      expect(find.text('...'), findsNothing);
+    });
+    testWidgets("Should find at least 1 ColorTile", (tester) async {
+      await tester.pumpWidget(const MyApp());
+      const loadingDuration = Duration(milliseconds: 500);
+      await tester.pump(loadingDuration);
+      expect(find.text('...'), findsNothing);
+      expect(find.text("Item 0"), findsOneWidget);
     });
   });
 }
