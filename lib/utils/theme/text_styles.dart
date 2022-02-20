99b729f155966ce2ea84dbf98a5ea7901a53cb6f
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-19 22:52:09 -0300

    adding method to get TextStyle by tag

--- lib/utils/theme/text_styles.dart
+++ lib/utils/theme/text_styles.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:testinflutter/utils/theme/text_styles_tags.dart';
 import 'package:testinflutter/utils/theme/ui_colors.dart';
 
 class TextStyles {
-  static final titleHome = GoogleFonts.lexendDeca(
-    fontSize: 32,
-    fontWeight: FontWeight.w600,
-    color: UiColors.heading,
-  );
-  static final titleNormal = GoogleFonts.lexendDeca(
-    fontSize: 16,
-    fontWeight: FontWeight.w600,
-    color: UiColors.heading,
-  );
+  static TextStyle getTextStyle(TextStylesTags tag) {
+    switch (tag) {
+      case TextStylesTags.titleHome:
+        return GoogleFonts.lexendDeca(
+          fontSize: 32,
+          fontWeight: FontWeight.w600,
+          color: UiColors.heading,
+        );
+      case TextStylesTags.titleNormal:
+        return GoogleFonts.lexendDeca(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: UiColors.heading,
+        );
+      default:
+        return const TextStyle();
+    }
+  }
 }
