084f6da49c5482c1fdc41cbf951bf7538b8c66bd
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-19 22:53:44 -0300

    setup fo Google fonts in homepage tests

--- test/Views/Homepage/home_page_test.dart
+++ test/Views/Homepage/home_page_test.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-import 'package:testinflutter/Views/Homepage/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:testinflutter/main.dart';
 
 void main() {
   group('HomePage testes ', () {
+    setUp(() => GoogleFonts.config.allowRuntimeFetching = false);
     testWidgets("founding at least 1 ColorTile", (tester) async {
-      // buildado widget
       await tester.pumpWidget(const MyApp());
-
       expect(find.text('Item 0'), findsWidgets);
       await tester.fling(find.byType(ListView), const Offset(0, -200), 3000);
       await tester.pumpAndSettle();
